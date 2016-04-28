be42b18ffd8842ff5bcc6e1b6856ba925fd3a545
thegoodfella <galas.daniele@gmail.com>
2016-04-28 16:12:32 +0200

    add --help tells you what operations are available

--- cose/Program.cs
+++ cose/Program.cs
@@ -11,6 +11,12 @@ namespace cose
         static void Main(string[] args)
         {
             Par p = new Par();
+
+            if (args[0] == "--help")
+            {
+                Console.Write("available operations:\n*\n+\n/");
+                return;
+            }
             Console.Write(p.CallFuncFromDic(args[0], args[2], args[1]));
         }
     }
